23a1bb03f518a7b8fec253778c6452d3db7c76a0
perlexed <perlexed@gmail.com>
2014-04-01 01:13:39 +0800

    local config

--- bootstrap.php
+++ bootstrap.php
@@ -1,5 +1,15 @@
 <?php
 
-require_once 'config.php';
+// @todo move to serv config
+define("IS_LOCAL", 1);
+
+
+// @todo move configs to git-free dir
+if(IS_LOCAL) {
+    require_once 'local_config.php';
+} else {
+    require_once 'config.php';
+}
+
 
 require_once 'modules/FileManager/FileManager.php';
